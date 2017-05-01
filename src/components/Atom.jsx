c60ed6be7e759f157faf23d54e6512f48d455327
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2017-05-01 11:10:45 +0100

    logic cof Atom

--- src/components/Atom.jsx
+++ src/components/Atom.jsx
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
+import { html } from 'common-tags';
 
 class Atom extends Component {
+  constructor() {
+    super();
+    this.renderSnippet = this.renderSnippet.bind(this);
+  }
+
+  renderSnippet(snippet) {
+    return html`
+      '${this.props.description}':
+        'prefix': '${this.props.tabtrigger}'
+        'body': """
+          ${snippet}
+        """
+    `;
+  }
+
   render() {
     return (
       <pre className="pre">
-        Very soon…
-
-        {/*{this.props.snippet}*/}
+        {this.renderSnippet(this.props.snippet)}
       </pre>
     );
   }
