cad8db2f446034e7a4acb4c24a666cfd058c3631
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2017-05-01 10:58:55 +0100

    Sublime Text logic

--- src/components/SublimeText.jsx
+++ src/components/SublimeText.jsx
@@ -1,12 +1,50 @@
 import React, { Component } from 'react';
+import { html } from 'common-tags';
 
 class SublimeText extends Component {
+  constructor() {
+    super();
+    this.renderSnippet = this.renderSnippet.bind(this);
+  }
+
+  renderSnippet(snippet) {
+    // escape " with \"
+    // split lines by line-break
+    // const separatedSnippet = snippet.replace(/"/g, '\\"').split('\n');
+    // const separatedSnippetLength = separatedSnippet.length;
+
+    // add double quotes around each line apart from the last one
+    // const newSnippet = separatedSnippet.map((line, index) => {
+    //   return index === separatedSnippetLength - 1 ? `"${line}"` : `"${line}",`;
+    // });
+
+    // return html`
+    //   "${this.props.description}": {
+    //     "prefix": "${this.props.tabtrigger}",
+    //     "body": [
+    //       ${newSnippet.join('\n')}
+    //     ],
+    //     "description": "${this.props.description}"
+    //   }
+    // `;
+
+    return html`
+      <snippet>
+        <content><![CDATA[
+      ${snippet}
+      ]]></content >
+        <description>${this.props.description}</description>
+        <tabTrigger>${this.props.tabtrigger}</tabTrigger>
+        <!-- Optional: Set a scope to limit where the snippet will trigger -->
+        <!-- <scope >source.python</scope > -->
+      </snippet >
+    `;
+  }
+
   render() {
     return (
       <pre className="pre">
-        Very soon…
-
-        {/*{this.props.snippet}*/}
+        {this.renderSnippet(this.props.snippet)}
       </pre>
     );
   }
