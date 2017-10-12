30a7d74a835a60707730b63e1e0edd59c49ef3dc
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2017-10-12 15:19:06 +0100

    add config

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
-import styles from './style.css';
+import './style.css';
 
 ReactDOM.render(<App />, document.getElementById('app'));
