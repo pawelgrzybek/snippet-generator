2b27aef10829f4115cef86abb30c721050a19037
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2018-03-19 17:24:56 +0000

    webpack 4 🎉

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  output: {
+    path: path.join(__dirname, 'docs'),
+    filename: 'bundle.js'
+  },
   resolve: {
     extensions: ['.js', '.jsx']
   },
