ae8574f9f6c383eb3952d4b18ab7ea37e0b1ae1e
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2018-05-25 11:40:46 +0100

    move dynamic bits to Context

--- src/components/Context.js
+++ src/components/Context.js
@@ -5,12 +5,16 @@ const context = createContext();
 const { Provider: ContextProvider, Consumer } = context;
 
 class Provider extends Component {
+  // state
   state = {
     description: '',
     tabTrigger: '',
     snippet: '',
     mode: 'vscode',
   }
+
+  // refs
+  textareaRef = React.createRef();
 
   onInput = e => {
     const { name, value } = e.target;
@@ -20,6 +24,7 @@ class Provider extends Component {
     }, this.generateURL);
   }
 
+  // my methods
   updateMode = mode => {
     if (mode === 'vscode') {
       document.documentElement.style.setProperty('--color', '#3B393C');
@@ -51,6 +56,41 @@ class Provider extends Component {
     }, document.title, `${location.pathname}?${shareUrl.searchParams}`);
   }
 
+  skipTabAndAddTwoSpaces(e) {
+    e.preventDefault();
+
+    const initialSelectrionStart = this.textareaRef.current.selectionStart;
+    const initialSelectrionEnd = this.textareaRef.current.selectionEnd;
+    const stringBeforeCaret = this.textareaRef.current.value.substr(0, initialSelectrionStart);
+    const stringAfterCaret = this.textareaRef.current.value.substr(initialSelectrionEnd, this.textareaRef.current.textLength);
+
+    const newValue = `${stringBeforeCaret}  ${stringAfterCaret}`;
+
+    this.textareaRef.current.value = newValue;
+    this.textareaRef.current.selectionStart = initialSelectrionStart + 2;
+    this.textareaRef.current.selectionEnd = initialSelectrionStart + 2;
+
+    this.onInput(e);
+  }
+
+  addPlaceHolder(e) {
+    e.preventDefault();
+
+    const initialSelectrionStart = this.textareaRef.current.selectionStart;
+    const initialSelectrionEnd = this.textareaRef.current.selectionEnd;
+    const stringBeforeCaret = this.textareaRef.current.value.substr(0, initialSelectrionStart);
+    const stringAfterCaret = this.textareaRef.current.value.substr(initialSelectrionEnd, this.textareaRef.current.textLength);
+
+    const newValue = `${stringBeforeCaret}\${1:example}${stringAfterCaret}`;
+
+    this.textareaRef.current.value = newValue;
+    this.textareaRef.current.selectionStart = initialSelectrionStart + 4;
+    this.textareaRef.current.selectionEnd = initialSelectrionStart + 11;
+
+    this.onInput(e);
+  }
+
+  // react lifecycle methods
   componentWillMount() {
     if ('URLSearchParams' in window === false) {
       return;
@@ -68,10 +108,24 @@ class Provider extends Component {
     });
   }
 
+  componentDidMount() {
+    window.addEventListener('keydown', e => {
+      // tab key (when snippet input is active)
+      if (e.keyCode === 9 && document.activeElement === this.textareaRef.current) {
+        this.skipTabAndAddTwoSpaces(e);
+      }
+      // Cmd + i (when snippet input is active)
+      if (e.keyCode === 73 && (e.ctrlKey || e.metaKey) && document.activeElement === this.textareaRef.current) {
+        this.addPlaceHolder(e);
+      }
+    });
+  }
+
   render() {
     return (
       <ContextProvider value={{
         state: this.state,
+        textareaRef: this.textareaRef,
         onInput: this.onInput,
         updateMode: this.updateMode,
         generateURL: this.generateURL,
