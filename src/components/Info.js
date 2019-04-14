648231f77190f39e1f5fc96386d685df6bc03998
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2019-04-14 09:35:13 +0100

    Fix some security issues

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,19 +1,33 @@
-import React from 'react';
-import { Consumer } from './Context';
+import React from "react";
+import { Consumer } from "./Context";
 
 const Info = () => (
   <Consumer>
     {context => {
       const docs = {
-        vscode: 'https://code.visualstudio.com/docs/editor/userdefinedsnippets',
-        sublimetext: 'http://docs.sublimetext.info/en/latest/extensibility/snippets.html',
-        atom: 'http://flight-manual.atom.io/using-atom/sections/snippets/',
+        vscode: "https://code.visualstudio.com/docs/editor/userdefinedsnippets",
+        sublimetext:
+          "http://docs.sublimetext.info/en/latest/extensibility/snippets.html",
+        atom: "http://flight-manual.atom.io/using-atom/sections/snippets/"
       };
 
-      const platformKey = navigator.platform === 'MacIntel' ? '⌘' : 'ctrl';
+      const platformKey = navigator.platform === "MacIntel" ? "⌘" : "ctrl";
 
       // eslint-disable-next-line no-template-curly-in-string
-      return <p className="app__info">To declare a placeholder ({ platformKey } + i): <span className="app__infoselect">{'${1:example}'}</span> | <a className="app__infolink" href={docs[context.state.mode]} target="_blank">More info</a></p>;
+      return (
+        <p className="app__info">
+          To declare a placeholder ({platformKey} + i):{" "}
+          <span className="app__infoselect">{"${1:example}"}</span> |{" "}
+          <a
+            className="app__infolink"
+            href={docs[context.state.mode]}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            More info
+          </a>
+        </p>
+      );
     }}
   </Consumer>
 );
